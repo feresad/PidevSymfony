75bc9ab13b4ebff4e0c9d93f201201a3827c7f8b
rayN3A7 <rayen.mannai@esprit.tn>
2025-04-22 23:15:56 +0100

    adding forum stats controller and page structure

--- src/Repository/CommentaireReactionsRepository.php
+++ src/Repository/CommentaireReactionsRepository.php
@@ -2,15 +2,15 @@
 
 namespace App\Repository;
 
-use App\Entity\Commentaire_reactions;
+use App\Entity\CommentaireReactions;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-class Commentaire_reactionsRepository extends ServiceEntityRepository
+class CommentaireReactionsRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Commentaire_reactions::class);
+        parent::__construct($registry, CommentaireReactions::class);
     }
 
     // Add custom methods as needed
