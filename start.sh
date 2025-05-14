7c0f97d6d67d03f4e9b1c73197d397574812b1a1
Adouani Feres <feresadouani10@gmail.com>
2025-05-14 01:59:30 +0100

    config2

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Vider le cache Symfony en mode production
 php bin/console cache:clear --env=prod --no-debug
-
+composer require symfony/runtime
 # Démarrer PHP-FPM en arrière-plan
 php-fpm &
 
