e7e20d505cd83b73e9d58c9394a8de6ba12f60cf
Adouani Feres <feresadouani10@gmail.com>
2025-05-15 16:25:09 +0100

    Modification env et Ajout du commande email

--- start.sh
+++ start.sh
@@ -10,6 +10,7 @@ chmod 664 /var/www/html/var/log/messenger.log
 cron
 # Démarrer PHP-FPM en arrière-plan
 php-fpm &
-
+# Démarrer le consommateur de messages en arrière-plan
+php bin/console messenger:consume async -vv &
 # Démarrer Nginx en avant-plan
 nginx -g 'daemon off;'
