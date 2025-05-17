570f413ad691ba214a722966e6ae8621a920de1c
Adouani Feres <feresadouani10@gmail.com>
2025-05-17 17:05:42 +0100

    Correction Commentaire Forum

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,8 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './vendor/summernote-bs4.min.js';
+import 'summernote/dist/summernote-bs4.min.js';
+import 'summernote/dist/summernote-bs4.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
